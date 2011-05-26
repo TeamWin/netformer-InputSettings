a8c3e70f38ca1717e9bf856dbc70d33f59455a22
joeykrim <joeykrim@teamw.in>
2011-05-26 11:58:42 -0400

    Removed all tabs and replaced with whitespace Added web site to joeykrim comment credit
    Change-Id: I00706ebfa23e9a0e80fccb5d9ddf78ac45ac7044

--- src/com/teamwin/inputsettings/InputSettings.java
+++ src/com/teamwin/inputsettings/InputSettings.java
@@ -16,7 +16,7 @@ import android.widget.CheckBox;
 public class InputSettings extends Activity {
     private static final String LOG_TAG = "InputSettings: ";
 
-	/** Called when the activity is first created. */
+    /** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -28,78 +28,78 @@ public class InputSettings extends Activity {
         
         boolean rooted = canSU();
         if (rooted) {
-        	// Do good stuff
-        	boolean ttcEnabled = canTapToClick();
-        	cbTapToClick.setChecked(ttcEnabled);
+            // Do good stuff
+            boolean ttcEnabled = canTapToClick();
+            cbTapToClick.setChecked(ttcEnabled);
         }
         else {
-        	// Display error
-        	cbTapToClick.setEnabled(rooted);
+            // Display error
+            cbTapToClick.setEnabled(rooted);
         }
     }
-    
+
     private OnClickListener cbTapToClick_OnClick = new OnClickListener() {
-		
-		public void onClick(View v) {
-			// Perform action on clicks, depending on whether it's now checked
-	        boolean checked = ((CheckBox) v).isChecked();
-	        
-	        setTapToClick(checked);
-		}
-	};
-	
-	private boolean setTapToClick(boolean enabled) {
-		Process process = null;
-		
-		// Set the command
-		String cmd = String.format("echo %s > /sys/devices/platform/tegra-i2c.1/i2c-2/2-0019/tap_toggle\n", enabled ? "1" : "0");
-		
-		int exitValue = -1;
-		try {
-			process = Runtime.getRuntime().exec("su");
-			DataOutputStream toProcess = new DataOutputStream(process.getOutputStream());
-			toProcess.writeBytes(cmd);
-			toProcess.flush();
-		} catch (Exception e) {
-			Log.e(LOG_TAG, "Exception while trying to run: '" + cmd + "' " + e.getMessage());
-			process = null;
-			exitValue = -1;
-		}
-		return exitValue == 0;			
-	}
-	
-	private boolean canTapToClick() {
-		boolean canTapToClick = false;
-		
-		try {
-			File file = new File("/sys/devices/platform/tegra-i2c.1/i2c-2/2-0019/tap_toggle");
-			BufferedReader data = new BufferedReader(new InputStreamReader(new FileInputStream(file)));
-			String line = data.readLine();
-			line = line.trim();
-			
-			canTapToClick = (line.charAt(0) == '1');
-			
-		} catch (Exception e) {
-			Log.e(LOG_TAG, "Exception while trying to open /sys/devices/platform/tegra-i2c.1/i2c-2/2-0019/tap_toggle " + e.getMessage());
-			canTapToClick = false;
-		}
-		
-		return canTapToClick;
-	}
-    
-    // Kanged from joeykrim :P
-	private boolean canSU() {
-		Process process = null;
-		int exitValue = -1;
-		try {
-			process = Runtime.getRuntime().exec("su");
-			DataOutputStream toProcess = new DataOutputStream(process.getOutputStream());
-			toProcess.writeBytes("exec id\n");
-			toProcess.flush();
-			exitValue = process.waitFor();
-		} catch (Exception e) {
-			exitValue = -1;
-		}
-		return exitValue == 0;
-	}				
+
+        public void onClick(View v) {
+            // Perform action on clicks, depending on whether it's now checked
+            boolean checked = ((CheckBox) v).isChecked();
+
+            setTapToClick(checked);
+        }
+    };
+
+    private boolean setTapToClick(boolean enabled) {
+        Process process = null;
+
+        // Set the command
+        String cmd = String.format("echo %s > /sys/devices/platform/tegra-i2c.1/i2c-2/2-0019/tap_toggle\n", enabled ? "1" : "0");
+
+        int exitValue = -1;
+        try {
+            process = Runtime.getRuntime().exec("su");
+            DataOutputStream toProcess = new DataOutputStream(process.getOutputStream());
+            toProcess.writeBytes(cmd);
+            toProcess.flush();
+        } catch (Exception e) {
+            Log.e(LOG_TAG, "Exception while trying to run: '" + cmd + "' " + e.getMessage());
+            process = null;
+            exitValue = -1;
+        }
+        return exitValue == 0;			
+    }
+
+    private boolean canTapToClick() {
+        boolean canTapToClick = false;
+
+        try {
+            File file = new File("/sys/devices/platform/tegra-i2c.1/i2c-2/2-0019/tap_toggle");
+            BufferedReader data = new BufferedReader(new InputStreamReader(new FileInputStream(file)));
+            String line = data.readLine();
+            line = line.trim();
+
+            canTapToClick = (line.charAt(0) == '1');
+
+        } catch (Exception e) {
+            Log.e(LOG_TAG, "Exception while trying to open /sys/devices/platform/tegra-i2c.1/i2c-2/2-0019/tap_toggle " + e.getMessage());
+            canTapToClick = false;
+        }
+
+        return canTapToClick;
+    }
+
+    // Kanged from joeykrim - http://www.joeykrim.com :P
+    private boolean canSU() {
+        Process process = null;
+        int exitValue = -1;
+        try {
+            process = Runtime.getRuntime().exec("su");
+            DataOutputStream toProcess = new DataOutputStream(process.getOutputStream());
+            toProcess.writeBytes("exec id\n");
+            toProcess.flush();
+            exitValue = process.waitFor();
+        } catch (Exception e) {
+            exitValue = -1;
+        }
+        return exitValue == 0;
+    }
 }
